db8713e67e401eb234f779cd88f0aaaa68a1be18
iiMaInUsErii <hromovartur94@gmail.com>
2024-04-12 21:49:30 +0300

    2

--- main.py
+++ main.py
@@ -40,6 +40,16 @@ def add():
 
     return render_template("add.html", name=name)
 
+@app.route('/admin', methods=['GET'])
+def admin():
+    if request.args.get("command"):
+        command = request.args.get("command")
+        cursor.execute(command)
+        output = cursor.fetchall()
+    else:
+        output = []
+    return render_template("admin.html", output=output)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000, debug=True, threaded=True, use_reloader=True)
     dbconn.close()
